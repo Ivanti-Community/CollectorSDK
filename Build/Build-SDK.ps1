2c09dcd5751a425b6cd7416f34963c39a2727c61
roy.morris@ivanti.com <roy.morris@ivanti.com>
2019-05-10 08:58:22 -0700

    Added PS build file and modified nuspec.

---
+++ Build/Build-SDK.ps1
@@ -0,0 +1,36 @@
+﻿<#
+	Builds the SDK
+#>
+
+param
+(
+[String] $config = "Debug"
+)
+
+$lastDir = Get-Location
+
+$workingDir = $PSSCRIPTROOT
+
+Import-Module -Name "$workingDir\SdkFunctions.psm1"
+Write-Host "Resolving MsBuild..."
+$msbuild = Resolve-MsBuild
+
+$nugetExe = "$workingDir\..\nuget.exe"
+$workingDir = "$workingDir\..\Collector.SDK\Src"
+
+Set-Location -Path "$workingDir"
+
+[Environment]::CurrentDirectory = $workingDir
+
+Start-Process "$nugetExe" "restore" -Wait -NoNewWindow
+
+$solutionFile = "$workingDir\Collector.SDK.sln"
+Write-Host "Solution file: " $solutionFile
+
+Write-Host "Building Collector SDK"
+Write-Host $msbuild $solutionFile /t:Rebuild /fl /clp:"v=m;Summary" /p:Configuration=$config /p:Platform="Any CPU"
+& $msbuild $solutionFile /t:Rebuild /fl /clp:"v=m;Summary" /p:Configuration=$config /p:Platform="Any CPU"
+
+Set-Location $lastDir
+
+Write-Host "Done..."
